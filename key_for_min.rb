a5aac15433050d477b86951eb80b8116e22e960e
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 23:58:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   return nil if name_hash == {}
   returned_key = ""
   value_array = name_hash.collect {|name, value| value}
-  name_hash.each_with_index do |(name, outer_value), index|
+  name_hash.each do |name, outer_value|
     returned_key = name if name_hash.all? {|name, value| outer_value <= value}
   end
   returned_key
