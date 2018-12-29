5dea2165205736ae40061e82030e4dae9ce592ad
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 21:39:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ testhash = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   name_hash.each do |key, value|
-    
+    name_hash.any? {|key, value| value }
   end
   value_array = name_hash.collect {|name, value| value}
   
