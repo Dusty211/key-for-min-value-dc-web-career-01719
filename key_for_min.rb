f28fbf5fdd06a3a8f9bcb8734e0ba9b6ecfe14e6
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 23:04:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   return nil if name_hash == {}
   value_array = name_hash.collect {|name, value| value}
   for i in (0..value_array.length - 1) do
-    puts name_hash.all? {|name, value| value <= value_array[i]}
+    puts name_hash.all? {|name, value| value_array[i] <= value}
     puts value_array[i]
   end
   
