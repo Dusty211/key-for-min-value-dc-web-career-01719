e13f1ca203cec4032e6c0d10d8f2232647ad8daf
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 23:55:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,14 +8,15 @@ def key_for_min_value(name_hash)
   return nil if name_hash == {}
   returned_key = ""
   value_array = name_hash.collect {|name, value| value}
-  name_hash.each_with_index do |(name, value), index|
-    returned_key = name if name_hash.all? {|name, value| value_array[i] <= value}
+  name_hash.each_with_index do |(name, outer_value), index|
+    returned_key = name if name_hash.all? {|name, value| outer_value <= value}
   end
-  value_array = name_hash.collect {|name, value| value}
-  for i in (0..value_array.length - 1) do
-    puts name_hash.all? {|name, value| value_array[i] <= value}
-    puts value_array[i]
-  end
+  returned_key
+  #value_array = name_hash.collect {|name, value| value}
+  #for i in (0..value_array.length - 1) do
+  #  puts name_hash.all? {|name, value| value_array[i] <= value}
+  #  puts value_array[i]
+  #end
   
 end
 
