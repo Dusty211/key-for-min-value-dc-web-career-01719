7d85d56803199b916d8bf9b7795f0239ee60f767
Kyle Houghton <kylebenz@hotmail.com>
2018-12-30 00:01:22 +0000

    All tests pass.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash == {}
+  returned_key = ""
+  value_array = name_hash.collect {|name, value| value}
+  name_hash.each do |name, outer_value|
+    returned_key = name if name_hash.all? {|name, value| outer_value <= value}
+  end
+  returned_key
 end
